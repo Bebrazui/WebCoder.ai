51a06e64a992fd0e8e9bc591c4b719c8d820ab2a
FlyBerry <flyberry.app@gmail.com>
2025-07-12 06:02:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/outline-view.tsx
+++ src/components/outline-view.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import type * as monaco from 'monaco-editor';
 import {
-  File, FunctionSquare, Building2, Variable, Box, Enum, Interface, Library, Property, EnumMember
+  File, FunctionSquare, Building2, Variable, Box, ListOrdered, Library, BookKey
 } from 'lucide-react';
 import { ScrollArea } from './ui/scroll-area';
 import { cn } from '@/lib/utils';
@@ -22,6 +22,25 @@ interface OutlineViewProps {
   onSymbolSelect: (range: monaco.IRange) => void;
 }
 
+const InterfaceIcon = (props: React.ComponentProps<'svg'>) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <circle cx="12" cy="12" r="10" />
+      <path d="M12 8v8" />
+      <path d="M9 12h6" />
+    </svg>
+  );
+
 const symbolKindToIcon: Record<monaco.languages.SymbolKind, React.ReactNode> = {
   [0]: <File className="h-4 w-4" />, // File
   [1]: <Library className="h-4 w-4 text-blue-400" />, // Module
@@ -29,11 +48,11 @@ const symbolKindToIcon: Record<monaco.languages.SymbolKind, React.ReactNode> = {
   [3]: <Library className="h-4 w-4 text-blue-400" />, // Package
   [4]: <Building2 className="h-4 w-4 text-amber-400" />, // Class
   [5]: <FunctionSquare className="h-4 w-4 text-purple-400" />, // Method
-  [6]: <Property className="h-4 w-4 text-teal-400" />, // Property
-  [7]: <FunctionSquare className="h-4 w-4 text-teal-400" />, // Field
+  [6]: <BookKey className="h-4 w-4 text-teal-400" />, // Property
+  [7]: <BookKey className="h-4 w-4 text-teal-400" />, // Field
   [8]: <FunctionSquare className="h-4 w-4 text-purple-400" />, // Constructor
-  [9]: <Enum className="h-4 w-4 text-amber-400" />, // Enum
-  [10]: <Interface className="h-4 w-4 text-cyan-400" />, // Interface
+  [9]: <ListOrdered className="h-4 w-4 text-amber-400" />, // Enum
+  [10]: <InterfaceIcon className="h-4 w-4 text-cyan-400" />, // Interface
   [11]: <FunctionSquare className="h-4 w-4 text-purple-400" />, // Function
   [12]: <Variable className="h-4 w-4 text-sky-400" />, // Variable
   [13]: <Variable className="h-4 w-4 text-sky-400" />, // Constant
@@ -44,7 +63,7 @@ const symbolKindToIcon: Record<monaco.languages.SymbolKind, React.ReactNode> = {
   [18]: <Variable className="h-4 w-4 text-sky-400" />, // Object
   [19]: <Variable className="h-4 w-4 text-sky-400" />, // Key
   [20]: <Variable className="h-4 w-4 text-sky-400" />, // Null
-  [21]: <EnumMember className="h-4 w-4 text-teal-400" />, // EnumMember
+  [21]: <Variable className="h-4 w-4 text-teal-400" />, // EnumMember
   [22]: <Box className="h-4 w-4 text-amber-400" />, // Struct
   [23]: <Variable className="h-4 w-4 text-sky-400" />, // Event
   [24]: <Variable className="h-4 w-4 text-sky-400" />, // Operator
